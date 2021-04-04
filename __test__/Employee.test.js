f6f9eb72a983a1fce096a7e2f88411284ee4c4ff
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-03 18:20:37 -0700

    add more test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,5 +1,13 @@
+const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('create new employee object', () => {
     const employee = new Employee('Rich');
-})
+});
+
+test('get employee ID info', () => {
+    const randomID = 1234;
+    const employee = new Employee('Rich', randomID);
+
+    expect(employee.id).toBe(randomID);
+});
