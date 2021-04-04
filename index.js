db2916924bdfd59801b2917f4816630145b18b2c
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-03 17:44:40 -0700

    add intern section

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const { truncate } = require('node:fs');
 
 
 
@@ -91,6 +92,130 @@ function init() {
                         return true;
                     } else {
                         console.log('Enter phone number!');
+                        return false;
+                    }
+                }
+            }
+        ])
+    }
+
+    // Engineer Section
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                // Engineer Name
+                type: 'input',
+                name: 'engineer',
+                message: "Enter engineer's name",
+                validate: engineerName => {
+                    if (engineerName) {
+                        return true;
+                    } else {
+                        console.log('Enter engineer name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                // Engineer ID
+                type: 'input',
+                name: 'EngineerId',
+                message: 'What is the engineer ID?',
+                validate: engineerID => {
+                    if (engineerID) {
+                        return true;
+                    } else {
+                        console.log('Enter engineer ID');
+                        return false;
+                    }
+                }
+            },
+            {
+                // Engineer email
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'Enter engineer email',
+                validate: engineerEmail => {
+                    if (engineerEmail) {
+                        return true;
+                    } else {
+                        console.log('Enter engineer email');
+                        return false;
+                    }
+                }
+            },
+            {
+                // GitHub section
+                type: 'input',
+                name: 'engineerGit',
+                message: "Enter engineer's GitHub username",
+                validate: engineerGit => {
+                    if (engineerGit) {
+                        return true;
+                    } else {
+                        console.log('Enter Engineer GitHub');
+                        return false;
+                    }
+                }
+            }
+        ])
+    }
+
+    // Intern Section
+    function addIntern() {
+        inquirer.prompt([
+            {
+                // Intern Name
+                type: 'input',
+                name: 'intern',
+                message: "Enter intern's name",
+                validate: internName => {
+                    if (internName) {
+                        return true;
+                    } else {
+                        console.log('Enter inter name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                // Intern ID
+                type: 'input',
+                name: 'internID',
+                message: 'Enter Intern ID',
+                validate: internID => {
+                    if (internID) {
+                        return true;
+                    } else {
+                        console.log('Enter Intern ID');
+                        return false;
+                    }
+                }
+            },
+            {
+                // Intern Email section
+                type: 'input',
+                name: 'internEmail',
+                message: 'Enter Intern email',
+                validate: internEmail => {
+                    if (internEmail) {
+                        return true;
+                    } else {
+                        console.log('Enter Intern Email');
+                        return false;
+                    }
+                }
+            },
+            {
+                // Intern GitHub section
+                type: 'input',
+                name: 'internGit',
+                message: 'Enter Inter GitHub Username',
+                validate: internGitHub => {
+                    if (internGitHub) {
+                        return true;
+                    } else {
+                        console.log('Enter Intern GitHub username');
                         return false;
                     }
                 }
