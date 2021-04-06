40f5ee24537c835e0bac069e99e17595e64e004d
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-05 18:41:50 -0700

    more test Engineer

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -4,3 +4,26 @@ const Engineer = require('../lib/Engineer');
 test('create an engineer object', () => {
     const engineer = new Engineer('Santos');
 });
+
+test('input a github account for engineer', () => {
+    const githubTest = 'santosGit';
+    const engineer = new Engineer('Santos', 1234, 'engineer@yahoo.com', githubTest);
+
+    expect(engineer.github).toBe(githubTest);
+});
+
+// testing getGitHub() function
+test('get the github info with function', () => {
+    const githubTest = 'santosGit';
+    const engineer = new Engineer('Santos', 1234, 'engineer@yahoo.com', githubTest);
+
+    expect(engineer.getGitHub()).toBe(githubTest);
+})
+
+// getRole section
+test('get Engineer role info', () => {
+    const testRole = 'Engineer';
+    const engineer = new Engineer('Santos', 1234, 'engineer@yahoo.com', 'santosGit', testRole);
+
+    expect(engineer.getRole()).toBe(testRole);
+});
