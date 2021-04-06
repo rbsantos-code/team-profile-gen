955809443cb13d4b201f3bc27ee7ee8b0db672e3
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-05 18:26:50 -0700

    pass and fail test engineer

---
+++ __test__/Engineer.test.js
@@ -0,0 +1,6 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+test('create an engineer object', () => {
+    const engineer = new Engineer('Santos');
+});
